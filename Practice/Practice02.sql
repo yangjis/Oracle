25701f4ee240e7e42402b40035598eb404716e10
hi <hi@DESKTOP-53P5LLN>
2020-06-09 10:38:43 +0900

    디비 그룹함수 과제

--- Practice/Practice02.sql
+++ Practice/Practice02.sql
@@ -88,8 +88,8 @@ order by avg(salary) desc;
 select employee_id, 
         hire_date,
         case when hire_date <= '02/12/31' then '창립맴버'
-             when hire_date <= '03/01/01' then '03년입사'
-             when hire_date <='04/01/01' then '04년입사'
+             when hire_date >= '03/01/01'  and hire_date < '04/01/01' then '03년입사'
+             when hire_date >='04/01/01' and hire_date <= '04/12/31' then '04년입사'
              else '상장이후입사'
         end optDate
 from employees
